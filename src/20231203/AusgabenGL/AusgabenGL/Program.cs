e67563ebc5c236f6b57b00f6cf0f31c7dc15bcd5
Iliyan <iliyanatanasov9@gmail.com>
2023-10-03 19:57:28 +0200

    Grundlagen zum Lernen und Testen

---
+++ src/20231203/AusgabenGL/AusgabenGL/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AusgabenGL
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Console.WriteLine("Name:Iliyan");
+            Console.WriteLine("Alter:   19 Jahre");
+            Console.WriteLine("Wohnort: Dornbirn");
+            Console.WriteLine("C# ist spanned zu Lernen");
+            Console.ReadLine();
+
+        }
+    }
+}
