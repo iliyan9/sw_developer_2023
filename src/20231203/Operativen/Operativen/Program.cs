da4a0fb3f8072f31f2e5a4c037b2697206d5dea5
Iliyan <iliyanatanasov9@gmail.com>
2023-10-03 21:23:38 +0200

    Start with arithetic operator

---
+++ src/20231203/Operativen/Operativen/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operatoren
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Arithmetische Operatoren
+            //+ - / * %
+
+            double flachRechteck = 0.0;
+            double seiteA = 0.0;
+            double seiteB = 0.0;
+
+            double flacheRechteck = (seiteA + seiteB) * 2;
+        }
+    }
+}
