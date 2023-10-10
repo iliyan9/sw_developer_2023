97000b13237734b28f59beec23ac587a782104a8
Iliyan <iliyanatanasov9@gmail.com>
2023-10-10 21:30:21 +0200

    Grundlagen Interationen

---
+++ src/20231010/Interation/Interation/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Interation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(i);
+
+            }
+            string input = string.Empty;
+
+            while (input.Length < 5)
+                do
+                {
+                    Console.Write("Ihre Eingabe: ");
+                    input = Console.ReadLine();
+                }
+                while (input.Length < 5);
+        }
+    }
+}
